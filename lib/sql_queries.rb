a72230190f10f48c36314b165d48f76857973b9a
Taishawn King <taishawnking1@yahoo.com>
2020-04-20 19:05:57 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -33,7 +33,7 @@ end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
   <<-SQL 
-   SELECT projects.title, Sum(pledge.amount) -  projects.funding_goal
+   SELECT projects.title, Sum(pledge.amount) - projects.funding_goal
    FROM Projects 
    JOIN pledge 
    ON
